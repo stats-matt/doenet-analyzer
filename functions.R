65fe19117a8758cbf3542516dd698085ffd7c38b
Matt Thomas <matt.thomas.math@gmail.com>
2022-07-18 12:00:02 -0400

    update for v 0.1.1 data

--- functions.R
+++ functions.R
@@ -10,15 +10,15 @@
 clean_events <- function(events,min_date,max_date) {
   
 #This block adds the timestamp column to the cleaned data set
-events <-
-  events %>%
-  group_by(userId) %>%
-  mutate(timestamp = anytime(timestamp)) %>%
-  mutate(time = timestamp - min(timestamp))%>%
-  ungroup()
+  events <-
+    events %>%
+    group_by(userId) %>%
+    mutate(timestamp = anytime(timestamp)) %>%
+    mutate(time = timestamp - min(timestamp)) %>%
+    ungroup()
 
 
-events<- events %>% filter(between(timestamp, min_date,max_date))
+  events <- events %>% filter(between(timestamp, min_date, max_date))
 
 
 events <-
@@ -49,12 +49,12 @@ events <-
 
 events$version_num = NA
 processed = events %>% group_by(activityCid) %>% summarize(min_stamp = min(timestamp))
-processed = processed[order(processed$min_stamp),]
+processed = processed[order(processed$min_stamp), ]
 dict = c(1:nrow(processed))
 names(dict) = processed$activityCid
-for(i in (1:(nrow(events)))){
-  working_id = events[[i,4]]
-  events[[i,ncol(events)]] = dict[working_id]
+for (i in (1:(nrow(events)))) {
+  working_id = events[[i, 4]]
+  events[[i, ncol(events)]] = dict[working_id]
 }
 
 
@@ -74,16 +74,18 @@ return(events)
 summarize_events <- function(data) {
   out <-
     data %>%
-    select(userId, starts_with("X"), time, timestamp, pageNumber, 
-           version_num,response,creditAchieved) %>%
-    group_by(userId, pageNumber, version_num) %>%
-    pivot_longer(cols = starts_with("X"),
-                 names_to = "problem",
-                 values_to = "score") %>%
-    ungroup() %>%
-    filter(score != -Inf) %>% 
-    group_by(problem,version_num) %>% 
-    mutate(avg = mean(score))%>% 
+    # select(userId, starts_with("X"), time, timestamp, pageNumber, 
+    #        version_num,response,creditAchieved) %>%
+    select(userId, item, time, timestamp, pageNumber, 
+           version_num,response,itemCreditAchieved, pageCreditAchieved) %>%
+    # group_by(userId, pageNumber, version_num) %>%
+    # pivot_longer(cols = starts_with("X"),
+    #              names_to = "problem",
+    #              values_to = "score") %>%
+    # ungroup() %>%
+    #filter(score != -Inf) %>% 
+    group_by(item,version_num) %>% 
+    mutate(avg = mean(itemCreditAchieved)) %>% 
     ungroup() %>%
     group_by(response) %>% 
     add_count(response) %>%
@@ -92,18 +94,18 @@ summarize_events <- function(data) {
   return(out)
 }
 #====================pull_dates=================================================
-pull_dates <- function(events){
-  out <- events%>%select(timestamp)
+pull_dates <- function(events) {
+  out <- events %>% select(timestamp)
   out <- anytime(out$timestamp)
   return(out)
 }
 #==================pull_versions================================================
-pull_versions <- function(events){
-  out <- events %>% distinct(activityCid)%>%nrow()
+pull_versions <- function(events) {
+  out <- events %>% distinct(activityCid) %>% nrow()
   return(out)
 }
 #================version_filter=================================================
-version_filter <- function(cleaned, input_version){
+version_filter <- function(cleaned, input_version) {
   out <- cleaned %>% filter(cleaned$version_num == input_version)
   return(out)
 }
