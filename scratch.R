93c82b3d9b3b601e6a7c0d5fd436d9060664df0e
Matt Thomas <matt.thomas.math@gmail.com>
2022-09-30 10:44:05 -0400

    fixes calculated times per person and activity
    also cleaned up some functions

--- scratch.R
+++ scratch.R
@@ -10,7 +10,7 @@ library(dplyr)
 source("functions.R")
 
 # load data (put in a doenetid - good doenetids to use are on slack)
-doenetid <- ""
+doenetid <- "_pdiqrEQqDLsTCucSaMdw1"
 raw <-  stream_in(file(
   paste0(
     "https://www.doenet.org/api/getEventData.php?doenetId[]=",
@@ -23,11 +23,19 @@ events <-  raw$events[[1]]
 dates <- pull_dates(events)
 min_date <- min(dates)
 max_date <- max(dates)
-cleaned_version <- clean_events(events, min(dates), max(dates))
-summary_data_version <- summarize_events(cleaned)
-cleaned <- version_filter(cleaned_version, 1)
+cleaned_versions <- clean_events(events, min(dates), max(dates))
+summary_data_version <- summarize_events(cleaned_versions)
+
+view(summary_data_version)
+
+cleaned <- version_filter(cleaned_versions, 1)
 summary_data <- summarize_events(cleaned)
 
 ###########################################
 ##### work below here
 ###########################################
+
+
+
+
+
