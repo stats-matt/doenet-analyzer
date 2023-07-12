a3e8037ed69e433f71e34f62a2a2fbc0765b1511
Jacob Levin <jrl359@cornell.edu>
2023-07-12 18:03:17 -0400

    testing committ

--- scratch.R
+++ scratch.R
@@ -38,4 +38,59 @@ summary_data <- summarize_events(cleaned)
 ###########################################
 
 
+# the function below extracts the exact id from the url within doenet
+library(stringr)
+
+extract_ids_code1 <- function(url) {
+  start_index <- str_locate(url, "data=")[,2]
+  ids <- list()
+  match_length <- attr(regexpr("data=", url), "match.length")
+  
+  while (start_index > -1) {
+    end_index <- str_locate(url, "&data=", start = start_index)[, 2]
+    if (end_index == -1) {
+      ids <- c(ids, list(str_sub(url, start_index)))
+      break
+    }
+    ids <- c(ids, list(str_sub(url, start_index, end_index - 1)))
+    start_index <- end_index + match_length
+  }
+  
+  return(ids)
+}
+extract_ids_code2 <- function(url) {
+  start_index <- regexpr("data=", url)[1] + 5
+  ids <- list()
+  match_length <- attr(regexpr("data=", url), "match.length")
+  
+  
+  while (start_index > 0) {
+    end_index <- regexpr("&data=", url, start_index)[1]
+    if (end_index == -1) {
+      ids <- c(ids, list(substr(url, 
+                                start_index)))
+      break
+    }
+    ids <- c(ids, list(substr(url, start_index, end_index - 1)))
+    start_index <- end_index + match_length
+  }
+  
+  return(ids)
+}
+extract_ids_code3 <- function(url) {
+  url_1 <- gsub("&code=.*", "", url)
+  url_2 <- sub("https://doenet.shinyapps.io/analyzer/\\?", "", url_1)
+  url_3 <- sub("data=", "&data=", url_2)
+  
+  ids <- as.list(strsplit(url_3, "&data=")[[1]][-1])
+  
+  return(ids)
+}
+
+url <- "https://doenet.shinyapps.io/analyzer/?data=_Y8rhJ0x5KzbEF4cc73RFH&data=_szGjThMMAaq0gmXaig9nq&code=4k6dSxGZ0BSztlexusbmU"
+ids <- extract_ids(url)
+print(ids)
+
+
+
 
